43873f29b0760395fcb5e2d48bfc6e08eadae13a
yousuf <muhammad.yousuf@tenpearls.com>
2020-02-12 13:08:05 +0500

    add unit test

---
+++ src/test/java/com/github/yousufdev/kafka/connect/smt/AppendSchemaTest.java
@@ -0,0 +1,47 @@
+package com.github.yousufdev.kafka.connect.smt;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class AppendSchemaTest {
+
+    private AppendSchema<SinkRecord> xform = new AppendSchema.Value<>();
+
+    @After
+    public void tearDown() { xform.close(); }
+
+
+    @Test
+    public void schemaless() {
+        String serializedSchema = "{'schema':{'type':'struct','fields':[{'type':'int32','optional':false,'field':'id'},{'type':'string','optional':false,'field':'name'}]}}";
+
+        final Map<String, Object> value = new HashMap<>();
+        value.put("id", 1);
+        value.put("name", "john");
+
+        xform.configure(Collections.singletonMap("schema", serializedSchema));
+
+        final SinkRecord record = new SinkRecord("", 0, null, null, null, value, 0);
+        final SinkRecord transformedRecord = xform.apply(record);
+
+        assertEquals(Schema.INT32_SCHEMA, transformedRecord.valueSchema().field("id").schema());
+        assertEquals(1, ((Struct) transformedRecord.value()).getInt32("id").intValue());
+
+        assertEquals(Schema.STRING_SCHEMA, transformedRecord.valueSchema().field("name").schema());
+        assertEquals("john", ((Struct) transformedRecord.value()).getString("name") );
+
+    }
+
+}
